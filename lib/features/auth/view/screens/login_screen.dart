4a7d5b67b43ad18fe264525ae92e9fe7468f3610
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-08-18 23:13:02 +0300

    - Added riverpod lints for analysis_options.yaml file - Added SnackBar widget - Added Loader widget - Create AuthViewModel class with riverpod generators - Create AuthRemoteRepository generators - Handel Loading, Success, Error state

--- lib/features/auth/view/screens/login_screen.dart
+++ lib/features/auth/view/screens/login_screen.dart
@@ -1,19 +1,23 @@
+import 'package:client/core/widgets/loader.dart';
 import 'package:client/features/auth/view/screens/signup_screen.dart';
+import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import '../../../../core/theme/app_palette.dart';
+import '../../../../core/utils/snack_bar.dart';
 import '../../../../core/widgets/custom_field.dart';
 import '../../repositories/auth_remote_repository.dart';
 import '../widgets/auth_gradient_button.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  ConsumerState<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends ConsumerState<LoginScreen> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final formKey = GlobalKey<FormState>();
@@ -27,84 +31,107 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authViewModelProvider)?.isLoading == true;
+    ref.listen(authViewModelProvider, (_, next) {
+      next?.when(
+        data: (data) {
+         // TODO navigate to home screen
+         //  Navigator.push(
+         //    context,
+         //    MaterialPageRoute(
+         //      builder: (context) => const LoginScreen(),
+         //    ),
+         //  );
+        },
+        error: (error, st) {
+          showSnackBar(
+            context: context,
+            message: error.toString(),
+          );
+        },
+        loading: () {},
+      );
+    });
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Form(
-          key: formKey,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 100),
-              child: Column(
-                children: [
-                  const Text(
-                    'Sign In.',
-                    style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  CustomField(
-                    hintText: 'Email',
-                    controller: emailController,
-                  ),
-                  const SizedBox(height: 15),
-                  CustomField(
-                    hintText: 'Password',
-                    controller: passwordController,
-                    isObscureText: true,
-                  ),
-                  const SizedBox(height: 20),
-                  AuthGradientButton(
-                    buttonText: 'Sign in',
-                    onTap: () async {
-                      if (formKey.currentState!.validate()) {
-                        final res = await AuthRemoteRepository().login(
-                          email: emailController.text,
-                          password: passwordController.text,
-                        );
-                        final val = switch (res) {
-                          Left(value: final l) => l,
-                          Right(value: final r) => r,
-                        };
-                        print(val);
-                      }
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SignupScreen(),
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Form(
+                key: formKey,
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 100),
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Sign In.',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      );
-                    },
-                    child: RichText(
-                      text: TextSpan(
-                        text: 'Don\'t have an account? ',
-                        style: Theme.of(context).textTheme.titleMedium,
-                        children: const [
-                          TextSpan(
-                            text: 'Sign Up',
-                            style: TextStyle(
-                              color: Palette.gradient2,
-                              fontWeight: FontWeight.bold,
+                        const SizedBox(height: 30),
+                        CustomField(
+                          hintText: 'Email',
+                          controller: emailController,
+                        ),
+                        const SizedBox(height: 15),
+                        CustomField(
+                          hintText: 'Password',
+                          controller: passwordController,
+                          isObscureText: true,
+                        ),
+                        const SizedBox(height: 20),
+                        AuthGradientButton(
+                          buttonText: 'Sign in',
+                          onTap: () async {
+                            if (formKey.currentState!.validate()) {
+                              final res = await AuthRemoteRepository().login(
+                                email: emailController.text,
+                                password: passwordController.text,
+                              );
+                              final val = switch (res) {
+                                Left(value: final l) => l,
+                                Right(value: final r) => r,
+                              };
+                              print(val);
+                            }
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const SignupScreen(),
+                              ),
+                            );
+                          },
+                          child: RichText(
+                            text: TextSpan(
+                              text: 'Don\'t have an account? ',
+                              style: Theme.of(context).textTheme.titleMedium,
+                              children: const [
+                                TextSpan(
+                                  text: 'Sign Up',
+                                  style: TextStyle(
+                                    color: Palette.gradient2,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
-                      ),
+                        )
+                      ],
                     ),
-                  )
-                ],
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
