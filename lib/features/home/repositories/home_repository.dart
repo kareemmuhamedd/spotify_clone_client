0b31650fb03cf518046ef2d6e5acb732d7bdc8a5
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-08-26 22:23:46 +0300

    - Added SongsScreen - Added LibraryScreen - Added SongModel - Added assets images png - Added Bottom Nav Bar in HomeScreen - Added Future Provider Function to getAllSongs in HomeViewmodel

--- lib/features/home/repositories/home_repository.dart
+++ lib/features/home/repositories/home_repository.dart
@@ -1,26 +1,28 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:client/core/constants/server_constants.dart';
 import 'package:client/core/failure/app_failure.dart';
+import 'package:client/features/home/model/song_model.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+
 part 'home_repository.g.dart';
+
 @riverpod
-HomeRepository homeRepository(HomeRepositoryRef ref){
+HomeRepository homeRepository(HomeRepositoryRef ref) {
   return HomeRepository();
 }
 
 class HomeRepository {
-  Future<Either<AppFailure, String>> uploadSong(
-  {
+  Future<Either<AppFailure, String>> uploadSong({
     required File selectedAudio,
     required File selectedThumbnail,
     required String songName,
     required String artist,
     required String hexCode,
     required String token,
-}
-  ) async {
+  }) async {
     try {
       final request = http.MultipartRequest(
         'POST',
@@ -57,6 +59,33 @@ class HomeRepository {
       }
 
       return Right(await res.stream.bytesToString());
+    } catch (e) {
+      return Left(AppFailure(e.toString()));
+    }
+  }
+
+  Future<Either<AppFailure, List<SongModel>>> getAllSongs({
+    required String token,
+  }) async {
+    try {
+      final res = await http.get(
+        headers: {
+          'Content-Type': 'application/json',
+          'x-auth-token': token,
+        },
+        Uri.parse('${ServerConstants.serverUrl}/song/list'),
+      );
+      var resBodyMap = jsonDecode(res.body);
+      if (res.statusCode != 200) {
+        resBodyMap = resBodyMap as Map<String, dynamic>;
+        return Left(AppFailure(resBodyMap['detail']));
+      }
+      resBodyMap = resBodyMap as List;
+      List<SongModel> songs = [];
+      for (final map in resBodyMap) {
+        songs.add( SongModel.fromMap(map));
+      }
+      return Right(songs);
     } catch (e) {
       return Left(AppFailure(e.toString()));
     }
